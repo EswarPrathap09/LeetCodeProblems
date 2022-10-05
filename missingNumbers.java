00a13fdd52ba0610c4605cfc11c73e2461a0286d
EswarPrathap09 <104545127+EswarPrathap09@users.noreply.github.com>
2022-10-05 11:25:25 +0530

    Add files via upload

---
+++ missingNumbers.java
@@ -0,0 +1,32 @@
+public class missingNumbers {
+    //https://leetcode.com/problems/missing-number/
+    //Cyclic Sort for sorting For Loop for finding.
+    public static void main(String[]args){
+        int [] arr ={4,0,2,1};
+        System.out.println(misNum(arr));
+    }
+
+    public static int misNum(int[] arr){
+        int i =0;
+        while(i<arr.length){
+            int correct=arr[i];
+            if(arr[i]<arr.length && arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+
+        for (int index =0;index < arr.length;index++){
+            if (arr[index]!=index){
+                return index;
+            }
+        }
+        return  arr.length;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp =arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
